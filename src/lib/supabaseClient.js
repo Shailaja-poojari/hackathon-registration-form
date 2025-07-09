55b2ff7b9acaf8d3439eb58305b98cc8fd3b79fc
Shailaja poojari <shailaja.s.poojary091@gmail.com>
2025-07-09 16:28:25 +0530

    Secure Supabase keys using .env file

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,6 @@
-// src/lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zkfuyzodvbgfiraryijh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprZnV5em9kdmJnZmlyYXJ5aWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5OTUxNDUsImV4cCI6MjA2NTU3MTE0NX0.6rCzqY5rylZgGBBZSZWR-4pbML3KOemrouPOkWprmek';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
